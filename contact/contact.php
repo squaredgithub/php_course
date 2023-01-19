79997a949c8cfb25f143fdb6c22560d87f2a3185
squaredgithub <njugunafrancis077@gmail.com>
2023-01-19 20:54:54 +0300

    php learning

---
+++ contact/contact.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST['submit'])) {
+    $name = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $subject = trim($_POST['subject']);
+    $message = trim($_POST['message']);
+
+    $myMail = "njugunafrancis077@gmail.com";
+    $header = "From: " . $email;
+    $message2 = "You have received a message from ".$name.". \n\n" . $message;
+
+    //1. Email your sending it to
+    //2. Subject
+    //3. Is the message
+
+    mail($myMail, $subject, $message2, $header);
+    header("Location: index.php?mailsend");
+}
+
+
+?>
---
+++ contact/index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div>
+    <h1>Get in Touch</h1>
+    <p>Please fill in the fields</p>
+    <form action="contact.php" method ="post">
+        <input type="text" name ="name" placeholder="Full Name">
+        <input type="text" name ="email" placeholder="E-mail">
+        <input type="text" name ="subject" placeholder="Subject">
+        <textarea name ="message" placeholder="Enter message"></textarea>
+        <button type="submit" name="submit">SEND</button>
+    </form>
+</div>
+</body>
+</html>
